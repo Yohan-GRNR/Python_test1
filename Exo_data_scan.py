220a807c4a94a9a03f211344345cffc1e7767300
Yohan-GRNR <y.grenier01@gmail.com>
2022-10-10 17:12:41 +0200

    Ajout de l'extraction de données sur un .csv

--- Exo_data_scan.py
+++ Exo_data_scan.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 url = "https://www.allocine.fr/film/meilleurs/"
 
@@ -7,13 +8,27 @@ response = requests.get(url)
 page = response.content
 soup = BeautifulSoup(page, "html.parser")
 
-titres = soup.find_all(class_="meta-title-link")
-for titre in titres:
-    print(titre.string)
+titres_bs = soup.find_all(class_="meta-title-link")
+titres = []
+for titre in titres_bs:
+    titres.append(titre.string)
 
-dates_sortie = soup.find_all(class_="date")
-for date in dates_sortie:
-    print(date.string)
+dates_sortie_bs = soup.find_all(class_="date")
+dates_de_sortie = []
+for date in dates_sortie_bs:
+    dates_de_sortie.append(date.string)
 
-#1. lier les 2 infos (colonne?)
+entete = ["Titre", "Date de sortie"]
+
+with open("database.csv", "w") as file_csv:
+    writer = csv.writer(file_csv, delimiter=",")
+    # Écrit la première ligne (l'entête)
+    writer.writerow(entete)
+    # Boucle les deux tableaux en quinconce
+    for titre, date in zip(titres, dates_de_sortie):
+        ligne=[titre, date]
+        # Écrit la ligne concerné à chaque boucle
+        writer.writerow(ligne)
+        
+
 #2. Attention titre sans date
