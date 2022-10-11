d8751582c997ce127a06f5ca56664e327f31ffd0
Yohan-GRNR <y.grenier01@gmail.com>
2022-10-11 14:02:30 +0200

    Ajout de la création du tableau + la récupération des données (manquantes) des dates

--- Exo_data_scan.py
+++ Exo_data_scan.py
@@ -1,6 +1,9 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
 import csv
+import re
 
 url = "https://www.allocine.fr/film/meilleurs/"
 
@@ -8,27 +11,39 @@ response = requests.get(url)
 page = response.content
 soup = BeautifulSoup(page, "html.parser")
 
-titres_bs = soup.find_all(class_="meta-title-link")
+films = soup.find_all(class_="meta-affintiy-score")
+regex_dates = r"<span class=\"date\">(.*)</span>"
+
 titres = []
+dates_de_sortie = []
+
+# Boucle sur la totalité des films pour ne pas rater ceux sans date
+for film in films:
+    date = re.findall(regex_dates, str(film))
+    # Si la valeur est null alors indiquer qu'il n'y a rien
+    if not date:
+        dates_de_sortie.append("Pas de date renseignée")
+    else:
+        dates_de_sortie.append(date)
+
+# Chaque film a forcément un titre
+titres_bs = soup.find_all(class_="meta-title-link")
 for titre in titres_bs:
     titres.append(titre.string)
 
-dates_sortie_bs = soup.find_all(class_="date")
-dates_de_sortie = []
-for date in dates_sortie_bs:
-    dates_de_sortie.append(date.string)
+print(titres)
+print(dates_de_sortie)
 
-entete = ["Titre", "Date de sortie"]
+# Création du tableau
+entete = ["Titre", "Date de reprise"]
 
 with open("database.csv", "w") as file_csv:
     writer = csv.writer(file_csv, delimiter=",")
     # Écrit la première ligne (l'entête)
     writer.writerow(entete)
+
     # Boucle les deux tableaux en quinconce
     for titre, date in zip(titres, dates_de_sortie):
         ligne=[titre, date]
         # Écrit la ligne concerné à chaque boucle
         writer.writerow(ligne)
-        
-
-#2. Attention titre sans date
